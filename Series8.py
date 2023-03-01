905120f9728120f5e007923b8c896e14e09d6a51
Soumita Das <soumitad991@gmail.com>
2023-03-01 21:19:22 +0530

    Fibonacci Series

--- Series8.py
+++ Series8.py
@@ -3,10 +3,8 @@
 n=int(input("Enter a number : "))
 a=0
 b=1
-print(a,end=" ")
-print(b,end=" ")
-for i in range(n-2):
+for i in range(n):
+    print(a,end=" ")
     count=a+b
-    print(count,end=" ")
     a=b
     b=count

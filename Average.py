b72990f86618c3dddd276d733a8c289a3bcb2097
Soumita Das <soumitad991@gmail.com>
2023-03-20 19:38:23 +0530

    Average of 3 inputted number

--- Average.py
+++ Average.py
@@ -1,5 +1,5 @@
-First_Num=input("Enter 1st number : ")
-Second_Num=input("Enter 2nd number : ")
-Third_Num=input("Enter 3rd number : ")
-Average=(int(First_Num)+int(Second_Num)+int(Third_Num))/3
-print("Average is = "+str(Average))
+num1=int(input("Enter 1st number : "))
+num2=int(input("Enter 2nd number : "))
+num3=int(input("Enter 3rd number : "))
+Avg=(num1+num2+num3)//3
+print("Average is : ",Avg)

41d92ad57372318b70e7fb7b5836af678ecff515
Soumita Das <soumitad991@gmail.com>
2023-04-01 22:02:31 +0530

    Recursion Using Module

--- RecursionModule.py
+++ RecursionModule.py
@@ -2,6 +2,16 @@
 def pow(base,exp):
     if exp==0:
         return 1
-    x=pow(base,exp-1)
-    y=base*x
-    return y
+    else:
+        x=pow(base,exp-1)
+        y=base*x
+        return y
+#gcd=greatest common divisior/Hcf=Highest common Factor
+#15=1,3,5,15
+#8=1,2,4,8
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        temp=a%b
+        return gcd(b,temp)
